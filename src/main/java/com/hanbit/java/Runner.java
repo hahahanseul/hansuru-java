9ffd3dc551cafe83497f97805931c2c6e144a3dd
11 <11@hb5018>
2016-12-06 14:52:17 +0900

    최초커밋

---
+++ src/main/java/com/hanbit/java/Runner.java
@@ -0,0 +1,18 @@
+package com.hanbit.java;
+
+import com.hanbit.animal.Dog;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		Dog dog = new Dog("도베르만", "검정");
+		Dog dog2 = new Dog("검둥이", "도베르만", "검정");
+		
+		System.out.println(dog);
+		System.out.println(dog2.getKind());
+		System.out.println(dog.getColor());
+		
+	}
+	
+	
+}
